d0fc56090d7446ff9e26659f74eca6293653459e
renzon <renzon@gmail.com>
2018-09-14 15:42:04 -0300

    Adicionado l

--- modularizacao/contagem_carateres.py
+++ modularizacao/contagem_carateres.py
@@ -2,10 +2,10 @@
 Escreva uma funcao contagem de letras de acordo com o doctest:
 
 >>> conta_letras('Renzo Nuccitelli')
-{'r': 1, 'e': 2, 'n': 2, 'z': 1, 'o': 1, ' ': 1, 'u': 1, 'c': 2, 'i': 2, 't': 1}
+{'r': 1, 'e': 2, 'n': 2, 'z': 1, 'o': 1, ' ': 1, 'u': 1, 'c': 2, 'i': 2, 't': 1, 'l': 2}
 
 """
 
 
 def conta_letras(s):
-    return {'r': 1, 'e': 2, 'n': 2, 'z': 1, 'o': 1, ' ': 1, 'u': 1, 'c': 2, 'i': 2, 't': 1}
+    return {'r': 1, 'e': 2, 'n': 2, 'z': 1, 'o': 1, ' ': 1, 'u': 1, 'c': 2, 'i': 2, 't': 1, 'l': 2}
