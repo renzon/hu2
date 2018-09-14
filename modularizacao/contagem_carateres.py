de20e183eaef2de98451a339946b693a33b62ca2
renzon <renzon@gmail.com>
2018-09-14 15:26:36 -0300

    Fiz vazio

--- modularizacao/contagem_carateres.py
+++ modularizacao/contagem_carateres.py
@@ -2,7 +2,7 @@
 Escreva uma funcao contagem de letras de acordo com o doctest:
 
 >>> conta_letras('Renzo Nuccitelli')
-{'r':1,'e':2, 'n':2, 'c':2, 'o':1, 'l':2, 'u':1, 'i':2, 't':1 }
+{'r':1,'e':2, 'n':2, 'c':2, 'o':1, 'l':2, 'u':1, 'i':2, 't':1, ' ':1 }
 
 """
 
