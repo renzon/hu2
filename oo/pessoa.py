6b135cd3382fa1ebe1b68f3a549e63eb7f4a0f3f
renzon <renzon@gmail.com>
2018-10-05 15:58:46 -0300

    Acesso a atributos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,41 @@
 class Pessoa:
     olhos = 2
 
-    def __init__(self, nome='Renzo'):
+    olhos+=1
+
+    def __init__(self, nome='Renzo', idade=36):
+        self._idade = idade
         self.nome = nome
+
+    @property
+    def idade(self):
+        return self._idade
+
+    @idade.setter
+    def idade(self, valor):
+        if valor<0:
+            raise ValueError('Valor não pode ser negativo')
+        self._idade = valor
+
+    def __str__(self):
+        return f'{self.nome}: {self.idade}'
 
 
 renzo = Pessoa()
-renzo.sobrenome = 'Nuccitelli'
-renzo.olhos = 3
-print(renzo.__dict__)
-print(Pessoa.olhos)
-del renzo.olhos
-print(renzo.__dict__)
-print(renzo.olhos)
-print(renzo.nome)
+
+# renzo.idade = -1
+# renzo.idade = -1
+print(renzo._idade)
+print(renzo.idade)
+print(renzo)
+
+# renzo.sobrenome = 'Nuccitelli'
+
+# renzo.olhos = 3
+# print(renzo.__dict__)
+# print(Pessoa.olhos)
+# del renzo.olhos
+# print(renzo.__dict__)
+# print(renzo.olhos)
+# print(renzo.nome)
 # print(Pessoa.nome)
